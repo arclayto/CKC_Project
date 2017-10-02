43bf657b0ff2fb8ec922832a385a366e8a95781b
austinc <claytoraustin@gmail.com>
2017-10-01 22:09:15 -0500

    preliminary physics/.gitignore testing

---
+++ Assets/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	public bool useOffset;
+
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		if (!useOffset) {
+			offset = target.position - transform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position - offset;
+
+		transform.LookAt (target);
+	}
+}
---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed;
+	public float jumpForce;
+	public float gravityForce;
+
+	private Vector3 moveDirection;
+	public CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		moveDirection = new Vector3 (Input.GetAxis ("Horizontal") * movementSpeed, moveDirection.y, Input.GetAxis ("Vertical") * movementSpeed);
+
+		if (Input.GetButtonDown ("Jump") && controller.isGrounded) {
+			moveDirection.y = jumpForce;
+		}
+
+		moveDirection.y = moveDirection.y + ((Physics.gravity.y * Time.deltaTime) * gravityForce);
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
