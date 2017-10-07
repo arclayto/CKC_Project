d7969404140edadae4f8a56b07d6c87fd32891e5
huedev <32443158+huedev@users.noreply.github.com>
2017-10-07 17:01:23 -0500

    Reworked fall animation, changed camera angle, added textures and improved lighting

---
+++ Assets/Scripts/TextureScale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScale : MonoBehaviour {
+
+	public float scale = 1;
+	Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		rend.material.mainTextureScale = new Vector2(transform.localScale.x * scale, transform.localScale.z * scale);
+	}
+}
