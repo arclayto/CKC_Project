357b72f72d7fddfdadb51f34581d3ae84379d8b1
huedev <32443158+huedev@users.noreply.github.com>
2017-10-10 00:45:10 -0500

    Added smoke effect to defeated enemies, a health pickup, tweaked aerial key spin, and slight tweaks to level layout

---
+++ Assets/Scripts/Misc/SmokeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeController : MonoBehaviour {
+
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update() {
+		if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f) {
+            // Animation complete, destroy gameobject
+            Destroy(gameObject);
+        }
+	}
+}
