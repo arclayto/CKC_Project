68a351d22b6870e333ef10955318272c0bdf4bad
austinc <claytoraustin@gmail.com>
2017-10-12 02:24:12 -0500

    Created the RisingPlatform Object
    #Platform remains invisible and collisonless until player gets close to it
    #Platform then animates up and fades in
    #Player can then jump on platform
    #When player gets far enough away, Platform then descends and fades to complete transparency

---
+++ Assets/Scripts/Objects/RisingPlatformController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingPlatformController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Animator animator = transform.GetComponentInChildren<Animator>();
+			animator.SetTrigger("PlatformUp");
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Animator animator = transform.GetComponentInChildren<Animator>();
+			animator.SetTrigger("PlatformDown");
+		}
+	}
+}
