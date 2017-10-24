dad51805b9125038ed9dabaaa6fa480ff39aded3
huedev <32443158+huedev@users.noreply.github.com>
2017-10-24 17:06:22 -0500

    Fixed starting platform skew, added tornado object

---
+++ Assets/Scripts/Objects/TornadoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoController : MonoBehaviour {
+
+	public float rotateSpeed = 100.0f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+	}
+}
