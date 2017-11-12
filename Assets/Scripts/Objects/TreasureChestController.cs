8a9391b022edc58dfad1068dedf5775f8891dced
huedev <32443158+huedev@users.noreply.github.com>
2017-11-12 00:55:04 -0600

    Added the treasure chest object (needs animation?)

---
+++ Assets/Scripts/Objects/TreasureChestController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChestController : MonoBehaviour {
+
+	public GameObject treasure;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player Attack") {
+	        treasure.SetActive(true);
+	        treasure.transform.parent = null;
+
+            // Enemy hit by key attack, destroy
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Player Attack") {
+	        treasure.SetActive(true);
+	        treasure.transform.parent = null;
+
+            // Enemy hit by key attack, destroy
+            Destroy(gameObject);
+        }
+    }
+}
