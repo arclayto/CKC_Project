766b8021b1b2b1512c1e43fe4d26ab15c9121305
Miguelectricity <miguelectricity@gmail.com>
2017-11-15 04:44:22 -0600

    Added Title Menu and Pause Menu
    Added base functionality for title menu and in-game pause menu

---
+++ Assets/Scripts/Pause Menu/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public UIManager UI;
+	public Canvas PauseCanvas;
+	public Canvas HUDCanvas;
+
+	private int isPaused;
+
+	// Use this for initialization
+	void Start () {
+		PauseCanvas.enabled = false;
+		HUDCanvas.enabled = true;
+		Time.timeScale = 1.0f;
+		isPaused = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TogglePauseMenu()
+	{
+		
+		if (isPaused == 0) 
+		{
+			PauseCanvas.enabled = true;
+			HUDCanvas.enabled = false;
+			Time.timeScale = 0.0f;
+			isPaused = 1;
+		}
+		else if (isPaused == 1) 
+		{
+			PauseCanvas.enabled = false;
+			HUDCanvas.enabled = true;
+			Time.timeScale = 1.0f;
+			isPaused = 0;
+		}
+	}
+
+	public void QuitToTitle(int code)
+	{
+		if (code == 1) {
+			GetComponent<LevelLoadTrigger>().initiateLoad("TitleScreen");
+		} else if (code == 2) {
+			
+			string scene = "TitleScreen";
+			AsyncOperation operation = SceneManager.LoadSceneAsync("Scenes/" + scene, LoadSceneMode.Single);
+			/*
+			while (operation.isDone == false) {
+				Debug.Log (operation.progress);
+				yield return null;
+			}
+			*/
+		}
+
+	}
+
+	public int getIsPaused()
+	{
+		return isPaused;
+	}
+}
---
+++ Assets/Scripts/Title Screen/TitleSunController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSunController : MonoBehaviour {
+
+	public Transform target;
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	void Update()
+	{
+		// Rotate the camera every frame so it keeps looking at the target
+		transform.LookAt(target);
+	}
+	// Update is called once per frame
+	void LateUpdate () {
+	//	transform.position = player.transform.position + offset;
+	}
+}
