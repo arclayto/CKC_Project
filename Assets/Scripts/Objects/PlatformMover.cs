503378565196978844fb2948d62500ecc7592266
austinc <claytoraustin@gmail.com>
2018-03-06 15:04:35 -0600

    Created Desert Stage Bonus Level, Sticking to Moving Platforms, And Platform Moving

---
+++ Assets/Scripts/Objects/PlatformMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour {
+
+	public Transform[] waypoints;
+	public float speed = 0.002f;
+	private int current = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (current >= waypoints.Length) {
+			current = 0;
+		}
+
+		if (transform.position.y != waypoints [current].transform.position.y) {
+			transform.position = Vector3.MoveTowards (transform.position, waypoints [current].transform.position, speed * Time.deltaTime);
+		}
+
+		if (transform.position.y == waypoints [current].transform.position.y) {
+			current++;
+		}
+	}
+}
---
+++ Assets/Scripts/Objects/PlatformStick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStick : MonoBehaviour {
+
+
+	private GameObject target = null;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		target = null;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider col){
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.transform.SetParent (gameObject.transform);
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		col.gameObject.transform.SetParent(null);
+	}
+		
+}
