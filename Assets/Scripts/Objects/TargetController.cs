91c3cccb0f81c6bd67db3fb7ea658570baaa136e
huedev <32443158+huedev@users.noreply.github.com>
2018-04-03 13:02:41 -0500

    Fixed prefab issues in Candyland, tweaked second target path, added smoke effect when target is destroyed

--- Assets/Scripts/Objects/TargetController.cs
+++ Assets/Scripts/Objects/TargetController.cs
@@ -7,7 +7,17 @@ public class TargetController : MonoBehaviour {
 	public GameObject target;
 
 	public void Activate() {
+		// Create smoke effect
+        MakeSmoke();
+
 		target.SetActive(true);
 		transform.parent.gameObject.SetActive(false);
 	}
+
+	public void MakeSmoke() {
+        // Create smoke effect
+        GameObject smoke = (GameObject)Instantiate(Resources.Load("Smoke"));
+        smoke.transform.position = transform.position;
+        smoke.transform.localScale = new Vector3(5f, 5f, 5f); 
+    }
 }
