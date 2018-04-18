a564b78c711f0db07d5444f0b980e17997b4b2dd
austinc <claytoraustin@gmail.com>
2018-04-18 09:20:35 -0500

    Transition from temple to canyland added

---
+++ Assets/Scripts/Objects/TempleDoorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempleDoorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player"){
+
+			StartCoroutine("FadeoutTimer");
+		}
+	}
+
+	IEnumerator FadeoutTimer() {
+		yield return new WaitForSeconds(1f);
+		GetComponent<LevelLoadTrigger>().initiateLoad();
+	}
+}
